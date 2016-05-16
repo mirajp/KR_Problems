faa016ea1f103fb131a14fe71236ea5a6370f2de
mirajp <mirage.patel@gmail.com>
2016-05-16 16:27:32 -0400

    Vertical bars for the histogram

--- Chapter1/histogram_wordlengths.c
+++ Chapter1/histogram_wordlengths.c
@@ -1,25 +1,58 @@
 #include <stdio.h>
 
 #define MAXWORDLENGTH 25
+#define BARWIDTH 3
 
-int lengthCounts[MAXWORDLENGTH+1];
+
+int lengthCounts[MAXWORDLENGTH];
 
 int main() {
-    int c, i, curLength = 0;
+    int c, i, j, curLength = 0, maxFreq = 0;
 
     while ((c = getchar()) != EOF) {
         if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
             curLength++;
         else {
-            lengthCounts[curLength]++;
-            curLength = 0;
+            if (curLength) {
+                lengthCounts[curLength-1]++;
+                curLength = 0;
+            }
         }
 
     }
 
-    for (i = 1; i <= MAXWORDLENGTH; i++) {
-        printf("Number of words of length %d: %d\n", i, lengthCounts[i]);
+    for (i = 0; i < MAXWORDLENGTH; i++) {
+        if (lengthCounts[i] > maxFreq)
+            maxFreq = lengthCounts[i];
+        //printf("Number of words of length %d: %d\n", i+1, lengthCounts[i]);
     }
-
+    
+    //printf("Highest freq: %d\n", maxFreq);
+    int charCount = 0;
+    printf("\t\t\t\tHistogram of word length frequencies\n");
+    for (j = maxFreq; j>= 1; j--) {
+        printf("%d\t\t", j);
+        for (i = 0; i < MAXWORDLENGTH; i++) {
+            if (lengthCounts[i] >= j) {
+                for (charCount = 0; charCount < BARWIDTH; charCount++)
+                    printf("\u2588");
+            }
+            else {
+                for (charCount = 0; charCount < BARWIDTH; charCount++)
+                    printf(" ");
+            }
+        }
+        printf("\n");
+    }
+    int length = 1;
+    
+    printf("Lengths:\t");
+    for (i = 0; i < MAXWORDLENGTH; i++) {
+        if (i+1 < 10)
+            printf(" %d ", i+1);
+        else
+            printf(" %d", i+1);
+    }
+    printf("\n");
     return 0;
 }
